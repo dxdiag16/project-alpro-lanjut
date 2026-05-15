27575e4903bf3882e7f8caba5cc2792a19c11d45
dxdiag16 <fachriqodri@gmail.com>
2026-05-15 21:18:06 +0700

    add a sell function and stack system to the inventory

--- inventory_and_shop_game.cpp
+++ inventory_and_shop_game.cpp
@@ -35,17 +35,19 @@ struct items {
     string type;
     int price;
     int quantity;
+    int maxStack;
 };
         
 items shopItems[] = {
-    {"Black Sword", "Weapon", "Long Sword", 500, 7},
-    {"Blood Reaper", "Weapon", "Scythe", 1000, 5},
-    {"Green Herb", "Consumable", "Heal", 100, 10},
+    {"Black Sword", "Weapon", "Long Sword", 500, 7, 1},
+    {"Blood Reaper", "Weapon", "Scythe", 1000, 5, 1},
+    {"Green Herb", "Consumable", "Heal", 100, 10, 5},
 };
 
 int num_shopItems = sizeof(shopItems)/sizeof(shopItems[0]);
 items inventoryItems[10];
 int inventoryCount = 0;
+int playerGold = 2000;
 
 int inputInt(string prompt) {
     string input;
@@ -94,10 +96,45 @@ void searchItems(){
 
 }
 
+bool addToInventory(items inventoryItems[], int &inventoryCount, int maxSize, items item, int amount) {
+   
+    int remaining = amount;
+    
+    for(int i = 0; i < inventoryCount; i++) {
+        if(inventoryItems[i].name == item.name && inventoryItems[i].quantity < item.maxStack) {
+            int space = item.maxStack - inventoryItems[i].quantity;
+            int add = min(space, remaining);
+
+            inventoryItems[i].quantity += add;
+            remaining -= add;
+
+            if(remaining == 0) return true;
+        }
+    }
+
+    while(remaining > 0) {
+        if(inventoryCount >= maxSize) {
+            return false;  
+        }
+
+        int add = min(item.maxStack, remaining);
+
+        inventoryItems[inventoryCount] = item;
+        inventoryItems[inventoryCount].quantity = add;
+
+        remaining -= add;
+        inventoryCount++;
+    }
+
+    return true;
+}   
+
 void buyItems(int *inventoryCount) {
     clearScreen();
     int choice, buyQuantity;
     bool isBuy = true;
+
+    cout << "Gold: " << playerGold << "G" << endl;
 
     viewItems(isBuy);
     choice = inputInt("Choose item number (0 to cancel): ");
@@ -124,24 +161,29 @@ void buyItems(int *inventoryCount) {
         return;
     }
 
-    if(*inventoryCount >= 10) {
+    int totalPrice = shopItems[index].price * buyQuantity;
+
+    if(playerGold < totalPrice) {
+        cout << "Not enough gold!" << endl;
+        return;
+    }
+    
+    bool success =  addToInventory(inventoryItems, *inventoryCount, 10, shopItems[index], buyQuantity);
+
+    if(!success) {
         cout << "Inventory full!" << endl;
         return;
     }
 
-    inventoryItems[*inventoryCount] = shopItems[index];
-    inventoryItems[*inventoryCount].quantity = buyQuantity;
-
+    playerGold -= totalPrice;
     shopItems[index].quantity -= buyQuantity;
-    (*inventoryCount)++;
-
     cout << "Item purchased!" << endl;
 }
 
 void viewInventory(int *inventoryCount) {
     clearScreen();
     if(*inventoryCount == 0) {
-        cout << "Inventory Kosong" << endl;
+        cout << "Inventory empty" << endl;
         waitAnyKey();
     } else {
         for(int i = 0; i < *inventoryCount; i++) {
@@ -150,7 +192,58 @@ void viewInventory(int *inventoryCount) {
                 << inventoryItems[i].quantity << endl;
         }
         waitAnyKey();
-    };
+    }
+}
+
+void removeEmptySlot(int &inventoryCount) {
+    for(int i = 0; i < inventoryCount; i++) {
+        if(inventoryItems[i].quantity == 0) {
+            for(int j = i; j < inventoryCount - 1; j++) {
+                inventoryItems[j] = inventoryItems[j + 1];
+            }
+            inventoryCount--;
+            i--;
+        }
+    }
+}
+
+void sellItems(int *inventoryCount) {
+    if(*inventoryCount == 0) {
+        cout << "Inventory empty" << endl;
+        return;
+    }
+
+    viewInventory(inventoryCount);
+
+    int choice = inputInt("Choose item number to sell (0 to cancel): ");
+
+    if(choice == 0) {
+        return;
+    }
+
+    if(choice < 1 || choice > *inventoryCount) {
+        cout << "Invalid item number!" << endl;
+        return;
+    }
+
+    int index = choice - 1;
+
+    int sellQuantity = inputInt("Enter quantity to sell (0 to cancel): ");
+
+    if(sellQuantity <= 0 || sellQuantity > inventoryItems[index].quantity) {
+        cout << "Invalid quantity!" << endl;
+        return;
+    }
+
+    int sellPrice = inventoryItems[index].price * 0.7;
+    int totalSell = sellPrice * sellQuantity;
+    playerGold += totalSell;
+    inventoryItems[index].quantity -= sellQuantity;
+
+    removeEmptySlot(*inventoryCount);
+
+    cout << "Item sold!" << endl;
+    cout << "Gold earned: " << totalSell << "G" << endl;
 }
 
 void shop(int *inventoryCount) {
@@ -180,7 +273,6 @@ void shop(int *inventoryCount) {
         }
     }while(option != 4);
     clearScreen();
-
 }
 
 void inventory(int *inventoryCount) {
@@ -191,7 +283,8 @@ void inventory(int *inventoryCount) {
         cout << "=== INVENTORY ===" << endl;
         cout << "1. View Inventory" << endl;
         cout << "2. Sort Items"     << endl;
-        cout << "3. Back"           << endl;
+        cout << "3. Sell Items"     << endl;
+        cout << "4. Back"           << endl;
         option = inputInt("Enter option: ");
 
         switch(option) {
@@ -200,16 +293,23 @@ void inventory(int *inventoryCount) {
                 break;
             case 2:
                 break;
+            case 3:
+                sellItems(inventoryCount);
+                break;
             default:
                 break;      
         }
-    }while(option != 3);
+    }while(option != 4);
     clearScreen();
 }
 
 int main() {
     int option;
     int inventoryCount = 0;
+
+    for(int i = 0; i < 10; i++) {
+        inventoryItems[i].quantity = 0;
+    }
 
     do {
         cout << "=== MANAGEMENT INVENTORY ERPEGE GAME === "  << endl;
@@ -236,6 +336,4 @@ int main() {
         }
     }while(option != 5);
     clearScreen();
-
-    cout << "Test";
 }
